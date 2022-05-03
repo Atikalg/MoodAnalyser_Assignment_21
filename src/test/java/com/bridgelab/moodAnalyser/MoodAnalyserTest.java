876527b4301ac89756e7f55de753554f24bccce3
atikalg <who.atik@gmail.com>
2022-05-03 20:04:55 +0530

    Added : Test case of mood analyser

---
+++ src/test/java/com/bridgelab/moodAnalyser/MoodAnalyserTest.java
@@ -0,0 +1,21 @@
+package com.bridgelab.moodAnalyser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+    MoodAnalyser mood1 = new MoodAnalyser("I am in happy Mood");
+    MoodAnalyser mood2 = new MoodAnalyser("I am in sad Mood");
+    //True test case for mood analyser
+    @Test
+    public void givenMessage_WhenProper_ShouldReturnHappy() {
+        String actual = mood1.analyseMood();
+        Assert.assertTrue("HAPPY", true);
+    }
+    //False test case for mood analyser
+    @Test
+    public void givenMessage_WhenProper_ShouldNotReturnHappy() {
+        String actual = mood2.analyseMood();
+        Assert.assertFalse("HAPPY", false);
+    }
+}
