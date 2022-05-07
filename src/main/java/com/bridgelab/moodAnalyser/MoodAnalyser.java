1e709f1bca3f9bd71811c1c2503f8787eb11b567
atikalg <who.atik@gmail.com>
2022-05-07 21:53:27 +0530

    Added : Mood analyser method

--- src/main/java/com/bridgelab/moodAnalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelab/moodAnalyser/MoodAnalyser.java
@@ -1,24 +1,13 @@
 package com.bridgelab.moodAnalyser;
 
-public class MoodAnalyser {
-    static String message;
-
-    public MoodAnalyser(String message) {
-        this.message = message;
+public class MoodAnalyser extends Exception{
+    public enum Exception_Type {
+        NULL, EMPTY
     }
+    public Exception_Type type;
 
-    public static String analyseMood() {
-        String messageOne = "I am in Sad Mood";
-        String messageTwo = "I am in any Mood";
-        try {
-            if (message.equalsIgnoreCase(messageOne))
-                return "SAD";
-            else if (message.equalsIgnoreCase(messageTwo))
-                return "HAPPY";
-            else
-                return "No Match";
-        } catch (NullPointerException e) {
-            return"HAPPY(null)";
-        }
+    public MoodAnalyser(String message, Exception_Type type) {
+        super(message);
+        this.type=type;
     }
 }
